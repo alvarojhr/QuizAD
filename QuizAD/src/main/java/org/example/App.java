f882bbe042da0163a6e3614bfc3f16c697ffa786
Alvaro Hernandez <alvarojhr96@gmail.com>
2023-04-10 01:44:50 -0500

    Ready to challenge!

---
+++ QuizAD/src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.example.controllers.GameController;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        GameController game = new GameController();
+        game.start();
+    }
+}
