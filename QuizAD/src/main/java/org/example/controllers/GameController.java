6ac3c7b5ed1f955c2d88e15a3c94c84eb966343d
Juan David Patiño <juandavidp1127@gmail.com>
2023-04-10 10:21:46 -0500

    add instructions

--- QuizAD/src/main/java/org/example/controllers/GameController.java
+++ QuizAD/src/main/java/org/example/controllers/GameController.java
@@ -57,6 +57,12 @@ public class GameController {
     }
 
     private void displayMenu() {
+        System.out.println("Reglas del juego: ");
+        System.out.println("El objetivo de cualquier mano de blackjack es derrotar a la banca.
+         Para esto, debes tener una mano que puntúe más alto que la mano de la banca, 
+         pero que no supere los 21 puntos en valor total. O bien, puedes ganar con una 
+         puntuación inferior a 22 cuando la mano de la banca supera los 21 puntos.");
+         
         System.out.println("\nMain Menu:");
         System.out.println("1. Play a round");
         System.out.println("2. Save game");
